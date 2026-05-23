9856aaa4a61481ef50ee03175dca64d3fd900476
Kritideepta Paul <your.email@example.com>
2026-05-24 00:30:15 +0530

    added Resume.js component

---
+++ src/Resume.js
@@ -0,0 +1,8 @@
+export default function Resume() {
+  return (
+    
+      My Developer Portfolio
+      The React site is successfully deployed! I will add my actual resume code here soon.
+    
+  );
+}
