5f9d8d3406c239b8298ccb86abbb8a00274ae346
Kritideepta Paul <your.email@example.com>
2026-05-24 00:33:01 +0530

    fixed Resume syntax

--- src/Resume.js
+++ src/Resume.js
@@ -1,8 +1,3 @@
 export default function Resume() {
-  return (
-    
-      My Developer Portfolio
-      The React site is successfully deployed! I will add my actual resume code here soon.
-    
-  );
+  return My Portfolio is Live!;
 }
